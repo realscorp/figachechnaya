0a780c5306d7727109d44eddc8809995610dbf0d
Sergey Krasnov <sa.krasnov@greenmoney.ru>
2022-09-30 12:17:31 +0700

    add /healthz for readinessProbe

--- microservices/history/app/main.py
+++ microservices/history/app/main.py
@@ -1,14 +1,20 @@
+import re
 from fastapi import FastAPI, Response, status
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import psycopg
+import os
+import time
 
+
+regexp_pattern = "[а-яА-Яеё,. ]+"
 history_count = 1
+system_is_ready = False
 db_host = 'localhost'
 db_port = '5432'
 db_name = 'history'
 db_login = 'history'
-db_pass = 'OneTwoTri4'
+db_pass = os.environ['DB_PASS']
 db_connection_string = ('host=' + db_host + 
                         ' port=' + db_port +
                         ' dbname=' + db_name +
@@ -22,15 +28,31 @@ class AppendRequest(BaseModel):
     figalized: str
 
 def init_table():
-    with psycopg.connect(db_connection_string) as conn:
-        with conn.cursor() as cur:
-            cur.execute(
-                '''CREATE TABLE IF NOT EXISTS HISTORY(
-                ID SERIAL PRIMARY KEY,
-                ORIGINAL TEXT,
-                FIGALIZED TEXT
-                )''')
+    global system_is_ready
+    print ('Initializing DB...')
+    try:
+        with psycopg.connect(db_connection_string) as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    '''CREATE TABLE IF NOT EXISTS HISTORY(
+                    ID SERIAL PRIMARY KEY,
+                    ORIGINAL TEXT,
+                    FIGALIZED TEXT
+                    )''')
+    except Exception:
+        print ('Cannot initialize DB')
+        system_is_ready = False
+        return system_is_ready
+    else:
+        print ('DB initialized')
+        system_is_ready = True
+        return system_is_ready
 
+# Проверка фразы на неверные символы
+def verify_phrase (phrase):
+    pattern = re.compile(regexp_pattern)
+    match = pattern.fullmatch(phrase)
+    return match
 
 # Создаём API-интерфейс
 app = FastAPI()
@@ -48,27 +70,58 @@ app.add_middleware(
 
 # Описываем обработку запроса к API добавления записи в историю
 @app.post("/api/append/", status_code=200)
-async def append_history(appendrequest: AppendRequest):
+async def append_history(appendrequest: AppendRequest, response: Response):
+    global system_is_ready
     print ('Incoming append request, original ' + appendrequest.original + ', figalized: ' + appendrequest.figalized)
-    with psycopg.connect(db_connection_string) as conn:
-        with conn.cursor() as cur:
-            cur.execute(
-                "INSERT INTO history (original, figalized) VALUES (%s, %s)",
-                (appendrequest.original, appendrequest.figalized))
+    if not (verify_phrase(appendrequest.original) and verify_phrase(appendrequest.original)):
+        print('Недопустимые символы')
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return
+    try:
+        with psycopg.connect(db_connection_string) as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    "INSERT INTO history (original, figalized) VALUES (%s, %s)",
+                    (appendrequest.original, appendrequest.figalized))
+    except:
+        print ('Cannot append to history')
+        system_is_ready = False
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return
+    else:
+        system_is_ready = True
+        return
 
 @app.get("/api/gethistory/", status_code=200)
-def get_history():
-    with psycopg.connect(db_connection_string) as conn:
-        with conn.cursor() as cur:
-            cur.execute(
-                "SELECT * FROM history ORDER BY id DESC LIMIT %s",
-                (history_count,))
-            result_list = [list(x) for x in cur.fetchall()]
-            return result_list
+def get_history(response: Response):
+    global system_is_ready
+    try:
+        with psycopg.connect(db_connection_string) as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    "SELECT * FROM history ORDER BY id DESC LIMIT %s",
+                    (history_count,))
+                result_list = [list(x) for x in cur.fetchall()]
+    except:
+        print ('Cannot get history')
+        system_is_ready = False
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return
+    else:
+        system_is_ready = True
+        return result_list
 
-# a : AppendRequest
-# a.original = 'велосипед'
-# a.figalized = 'фигасипед'
-init_table()
-# append_history (a)
-# print(get_history(11))
+# readinessProbe для Кубернетес
+@app.get("/api/healthz/")
+def get_history(response: Response):
+    global system_is_ready
+    if system_is_ready:
+        response.status_code = status.HTTP_200_OK
+        return
+    else:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return
+
+
+# while not init_table():
+#     time.sleep(1)
