68163037856d07da985f947f6312609177456031
Sergey Krasnov <sa.krasnov@greenmoney.ru>
2022-09-30 12:35:47 +0700

    add comments

--- microservices/history/app/main.py
+++ microservices/history/app/main.py
@@ -6,7 +6,6 @@ import psycopg
 import os
 import time
 
-
 regexp_pattern = "[а-яА-Яеё,. ]+"
 history_count = 1
 system_is_ready = False
@@ -14,6 +13,7 @@ db_host = 'localhost'
 db_port = '5432'
 db_name = 'history'
 db_login = 'history'
+# Секрет получаем из переменных окружения контейнера
 db_pass = os.environ['DB_PASS']
 db_connection_string = ('host=' + db_host + 
                         ' port=' + db_port +
@@ -73,16 +73,19 @@ app.add_middleware(
 async def append_history(appendrequest: AppendRequest, response: Response):
     global system_is_ready
     print ('Incoming append request, original ' + appendrequest.original + ', figalized: ' + appendrequest.figalized)
+    # Так как API будет доступно снаружи, делаем проверку на лишние символы для безопасности
     if not (verify_phrase(appendrequest.original) and verify_phrase(appendrequest.original)):
         print('Недопустимые символы')
         response.status_code = status.HTTP_400_BAD_REQUEST
         return
+    # Делаем вставку в таблицу переданных значений
     try:
         with psycopg.connect(db_connection_string) as conn:
             with conn.cursor() as cur:
                 cur.execute(
                     "INSERT INTO history (original, figalized) VALUES (%s, %s)",
                     (appendrequest.original, appendrequest.figalized))
+    # В случае сбоя выставляем флаг неготовности системы
     except:
         print ('Cannot append to history')
         system_is_ready = False
@@ -91,10 +94,11 @@ async def append_history(appendrequest: AppendRequest, response: Response):
     else:
         system_is_ready = True
         return
-
+# API для запроса списка преобразованных слов из истории. У нас его использует фронтенд для генерации подсказки и отображения статистики
 @app.get("/api/gethistory/", status_code=200)
 def get_history(response: Response):
     global system_is_ready
+    # Пробуем выполнить запрос к БД
     try:
         with psycopg.connect(db_connection_string) as conn:
             with conn.cursor() as cur:
@@ -102,6 +106,7 @@ def get_history(response: Response):
                     "SELECT * FROM history ORDER BY id DESC LIMIT %s",
                     (history_count,))
                 result_list = [list(x) for x in cur.fetchall()]
+    # Если не получилось, выставляем флаг неготовности системы
     except:
         print ('Cannot get history')
         system_is_ready = False
@@ -122,5 +127,7 @@ def get_history(response: Response):
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
         return
 
+# На старте постоянно пытаемся выполнить инициализацию таблицы
+# Пока не получится, флаг готовности системы будет false
 while not init_table():
     time.sleep(1)
