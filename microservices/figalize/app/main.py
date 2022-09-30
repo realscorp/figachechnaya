212503c383f57a6019d8180b79e0c94287227570
Sergey Krasnov <sa.krasnov@greenmoney.ru>
2022-09-30 14:19:53 +0700

    add metrics export

--- microservices/figalize/app/main.py
+++ microservices/figalize/app/main.py
@@ -4,11 +4,18 @@ from fastapi import FastAPI, Response, status
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import requests
+from prometheus_client import start_http_server, Summary, Counter, Gauge, Histogram
 
 system_is_ready = False
 regexp_pattern = "[а-яА-Яеё,. ]+"
 json_path = '/var/config/example.json'
 append_history_url = 'http://localhost:9000/api/append/'
+
+# Инициализируем каунтеры для экспорта метрик
+figalize_latency_seconds = Histogram('figalize_latency_seconds', 'Latency histogram for Figalize request processing')
+figalize_count = Counter('figalize_count', 'Number of times Figalize requests was processed',['result'])
+get_schemas_latency_seconds = Histogram('get_schemas_latency_seconds', 'Latency histogram for get Schemas request processing')
+get_schemas_count = Counter('get_schemas_success_count', 'Number of times get Schemas requests was processed',['result'])
 
 # Создаём свой тип данных, чтобы загружать в него данные из запроса
 class Request(BaseModel):
@@ -78,30 +85,35 @@ app.add_middleware(
 # Описываем обработку запроса к API фигализации
 @app.post("/api/figalize/", status_code=200)
 async def api_figalize_phrase(request: Request, response: Response):
-    figalized_result = ''
-    print('Incoming request:', request)
-    if verify_phrase(request.phrase):
-        for phrase_word in request.phrase.split(' '):
-            figalized_result += (figalize (phrase_word,schemas[request.schema_id]['substitutions']) + ' ')
+    with figalize_latency_seconds.time():
+        figalized_result = ''
+        print('Incoming request:', request)
+        if verify_phrase(request.phrase):
+            for phrase_word in request.phrase.split(' '):
+                figalized_result += (figalize (phrase_word,schemas[request.schema_id]['substitutions']) + ' ')
+            else:
+                history_data = (json.dumps(({'original':request.phrase, 'figalized':figalized_result}), indent = 4, ensure_ascii=False)).encode('utf-8').decode('unicode-escape')
+                print (history_data)
+                requests.post(append_history_url, data = history_data)
+                figalize_count.labels('success').inc()
+                return {'data': figalized_result}
         else:
-            history_data = (json.dumps(({'original':request.phrase, 'figalized':figalized_result}), indent = 4, ensure_ascii=False)).encode('utf-8').decode('unicode-escape')
-            print (history_data)
-            requests.post(append_history_url, data = history_data)
-            return {'data': figalized_result}
-    else:
-        # Если фраза содержит неверные символы, возвращаем код 400 и сообщение об ошибке
-        response.status_code = status.HTTP_400_BAD_REQUEST
-        return {'data':'По-русски, пожалуйста, у нас тут культурное заведение'}
+            # Если фраза содержит неверные символы, возвращаем код 400 и сообщение об ошибке
+            figalize_count.labels('fail').inc()
+            response.status_code = status.HTTP_400_BAD_REQUEST
+            return {'data':'По-русски, пожалуйста, у нас тут культурное заведение'}
 
 # Описываем обработку запроса списка схем
 @app.get("/api/getschemas/", status_code=200)
 async def api_getschemas():
-    count = 0
-    schema_list = {}
-    for schema in schemas:
-        schema_list[count]=schema['name']
-        count += 1
-    return schema_list
+    with get_schemas_latency_seconds.time():
+        count = 0
+        schema_list = {}
+        for schema in schemas:
+            schema_list[count]=schema['name']
+            count += 1
+        get_schemas_count.labels('success').inc()
+        return schema_list
 
 # readinessProbe для Кубернетес
 @app.get("/api/healthz/")
@@ -116,3 +128,4 @@ def get_history(response: Response):
 
 # Подгружаем схемы фигализации из файла
 schemas = load_schemas(json_path)
+start_http_server(9091)
