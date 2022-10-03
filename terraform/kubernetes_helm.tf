a1e28abfc43b6f6f555033e7bccbdf24f3c1232b
Sergey Krasnov <sa.krasnov@greenmoney.ru>
2022-10-03 21:10:53 +0700

    revert strict dependency back

--- terraform/kubernetes_helm.tf
+++ terraform/kubernetes_helm.tf
@@ -11,10 +11,5 @@ resource "helm_release" "certmanager" {
         value = "true"
     }
     
-    kubernetes {
-        host = yamldecode(data.mcs_kubernetes_cluster.cluster.k8s_config).clusters[0].cluster.server
-        cluster_ca_certificate = base64(yamldecode(data.mcs_kubernetes_cluster.cluster.k8s_config).clusters[0].cluster.certificate-authority-data)
-        client_certificate = base64(yamldecode(data.mcs_kubernetes_cluster.cluster.k8s_config).users[0].user.client-certificate-data)
-        client_key = base64(yamldecode(data.mcs_kubernetes_cluster.cluster.k8s_config).users[0].user.client-key-data)
-    }
+    depends_on = [mcs_kubernetes_node_group.nodegroup]
 }
