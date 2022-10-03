ec6b6f84356103dd0e3d358fafa1db20f387cb5b
Sergey Krasnov <sa.krasnov@greenmoney.ru>
2022-10-03 23:13:19 +0700

    install kube-prometheus-stack

--- terraform/kubernetes_helm.tf
+++ terraform/kubernetes_helm.tf
@@ -13,3 +13,20 @@ resource "helm_release" "certmanager" {
     
     depends_on = [mcs_kubernetes_node_group.nodegroup]
 }
+
+resource "helm_release" "kube-prometheus-stack" {
+    name       = "kube-prometheus-stack"
+    repository = "https://prometheus-community.github.io/helm-charts"
+    chart      = "kube-prometheus-stack"
+    version    = "40.3.1"
+    set {
+        name = "alertmanagerSpec.externalUrl"
+        value = "https://figachechnaya.ru/alertmanager"
+    }
+    set {
+        name = "prometheusSpec.externalUrl"
+        value = "https://figachechnaya.ru/prometheus"
+    }
+
+    depends_on = [mcs_kubernetes_node_group.nodegroup]
+}
