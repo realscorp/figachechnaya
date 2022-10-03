a471295f76ca8d47b874a8007704eba4e41119d3
Sergey Krasnov <sa.krasnov@greenmoney.ru>
2022-10-03 21:50:58 +0700

    remove unused bash scripts

--- kubernetes/helm/certmanager/install_certmanager.sh
+++
@@ -1,7 +0,0 @@
-# Пока нет пайплайна
-helm install \
-  cert-manager jetstack/cert-manager \
-  --namespace cert-manager \
-  --create-namespace \
-  --version v1.9.1 \
-  --set installCRDs=true
--- kubernetes/manifests/create_secrets.sh
+++
@@ -1,3 +0,0 @@
-kubectl create secret generic history \
-  --from-literal=db_login=$DB_LOGIN \
-  --from-literal=db_password=$DB_PASS
