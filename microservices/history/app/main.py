1b2081c3490242fbd4fd27a9994bf27c22a98c4d
Sergey Krasnov <sa.krasnov@greenmoney.ru>
2022-10-05 12:23:39 +0700

    migrate to stralett_exporter for metrics

--- microservices/history/app/main.py
+++ microservices/history/app/main.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 import psycopg
 import os
 import time
-from prometheus_client import start_http_server, Summary, Counter, Gauge, Histogram
+from starlette_exporter import PrometheusMiddleware, handle_metrics
 
 regexp_pattern = "[а-яА-Яеё,. ]+"
 # Глубина запроса к истории
@@ -17,7 +17,6 @@ db_port = os.environ['DB_PORT']
 db_name = os.environ['DB_NAME']
 db_login = os.environ['DB_LOGIN']
 db_pass = os.environ['DB_PASS']
-metrics_port = os.environ['METRICS_PORT_HISTORY']
 # Собираем строку подключения для удобства
 db_connection_string = ('host=' + db_host + 
                         ' port=' + db_port +
@@ -25,12 +24,6 @@ db_connection_string = ('host=' + db_host +
                         ' user=' + db_login +
                         ' password=' + db_pass
                         )
-
-# Инициализируем каунтеры для экспорта метрик
-history_appends_latency_seconds = Histogram('history_appends_latency_seconds', 'Latency histogram for History append request processing')
-history_appends_count = Counter('history_appends_count', 'Number of times History append requests was processed',['result'])
-history_get_latency_seconds = Histogram('history_get_latency_seconds', 'Latency histogram for get History request processing')
-history_get_count = Counter('history_get_success_count', 'Number of times get History requests was processed',['result'])
 
 # Создаём свой тип данных, чтобы загружать в него данные из запроса
 class AppendRequest(BaseModel):
@@ -81,64 +74,73 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+# Создаём экспортер метрик
+app.add_middleware(
+    PrometheusMiddleware,
+    app_name="history",
+    prefix='history',
+    labels={
+        "server_name": os.getenv("HOSTNAME"),
+    },
+    group_paths=True,
+    buckets=[0.025, 0.037, 0.05, 0.064, 0.079, 0.096, 0.115, 0.135, 0.157, 0.181, 0.208, 0.238, 0.27, 0.305, 0.344, 0.386, 0.432, 0.483, 0.539, 0.6, 0.667, 0.741, 0.822, 0.91, 1.007, 1.113, 1.23, 1.358, 1.498, 1.651, 1.82, 2.004, 2.207, 2.428, 2.672, 2.938],
+    skip_paths=[
+        '/api/healthz',
+        '/metrics'],
+    always_use_int_status=False
+    )
+app.add_route("/metrics", handle_metrics)
 
 # Описываем обработку запроса к API добавления записи в историю
 @app.post("/api/append/", status_code=200)
 async def append_history(appendrequest: AppendRequest, response: Response):
-    with history_appends_latency_seconds.time():
-        # Получаем доступ к переменной-флагу, объявленной глобально
-        global system_is_ready
-        # Отладочная информация в консоль
-        print ('Incoming append request, original ' + appendrequest.original + ', figalized: ' + appendrequest.figalized)
-        # Так как API будет доступно снаружи, делаем проверку на лишние символы для безопасности
-        if not (verify_phrase(appendrequest.original) and verify_phrase(appendrequest.figalized)):
-            print('Недопустимые символы')
-            history_appends_count.labels('fail').inc()
-            response.status_code = status.HTTP_400_BAD_REQUEST
-            return
-        # Делаем вставку в таблицу переданных значений
-        try:
-            with psycopg.connect(db_connection_string) as conn:
-                with conn.cursor() as cur:
-                    cur.execute(
-                        "INSERT INTO history (original, figalized) VALUES (%s, %s)",
-                        (appendrequest.original, appendrequest.figalized))
-        # В случае сбоя выставляем флаг неготовности системы
-        except:
-            print ('Cannot append to history')
-            system_is_ready = False
-            history_appends_count.labels('fail').inc()
-            response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-            return
-        else:
-            history_appends_count.labels('success').inc()
-            system_is_ready = True
-            return
+    # Получаем доступ к переменной-флагу, объявленной глобально
+    global system_is_ready
+    # Отладочная информация в консоль
+    print ('Incoming append request, original ' + appendrequest.original + ', figalized: ' + appendrequest.figalized)
+    # Так как API будет доступно снаружи, делаем проверку на лишние символы для безопасности
+    if not (verify_phrase(appendrequest.original) and verify_phrase(appendrequest.figalized)):
+        print('Недопустимые символы')
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return
+    # Делаем вставку в таблицу переданных значений
+    try:
+        with psycopg.connect(db_connection_string) as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    "INSERT INTO history (original, figalized) VALUES (%s, %s)",
+                    (appendrequest.original, appendrequest.figalized))
+    # В случае сбоя выставляем флаг неготовности системы
+    except:
+        print ('Cannot append to history')
+        system_is_ready = False
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return
+    else:
+        system_is_ready = True
+        return
 
 # API для запроса списка преобразованных слов из истории. У нас его использует фронтенд для генерации подсказки и отображения статистики
 @app.get("/api/gethistory/", status_code=200)
 def get_history(response: Response):
-    with history_get_latency_seconds.time():
-        global system_is_ready
-        # Пробуем выполнить запрос к БД
-        try:
-            with psycopg.connect(db_connection_string) as conn:
-                with conn.cursor() as cur:
-                    cur.execute(
-                        "SELECT * FROM history ORDER BY id DESC LIMIT %s",
-                        (history_count,))
-                    result_list = [list(x) for x in cur.fetchall()]
-        # Если не получилось, выставляем флаг неготовности системы
-        except:
-            print ('Cannot get history')
-            system_is_ready = False
-            history_get_count.labels('fail').inc()
-            response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-            return
-        else:
-            history_get_count.labels('success').inc()
-            system_is_ready = True
-            return result_list
+    global system_is_ready
+    # Пробуем выполнить запрос к БД
+    try:
+        with psycopg.connect(db_connection_string) as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    "SELECT * FROM history ORDER BY id DESC LIMIT %s",
+                    (history_count,))
+                result_list = [list(x) for x in cur.fetchall()]
+    # Если не получилось, выставляем флаг неготовности системы
+    except:
+        print ('Cannot get history')
+        system_is_ready = False
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return
+    else:
+        system_is_ready = True
+        return result_list
 
 # readinessProbe для Кубернетес
 @app.get("/api/healthz/")
@@ -153,6 +155,5 @@ def get_history(response: Response):
 
 # На старте постоянно пытаемся выполнить инициализацию таблицы
 # Пока не получится, флаг готовности системы будет false
-while not init_table():
-    time.sleep(1)
-start_http_server(int(metrics_port))
+# while not init_table():
+#     time.sleep(1)
