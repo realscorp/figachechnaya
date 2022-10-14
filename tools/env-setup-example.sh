c8635215977b44eb3dcdd2fc36838d507c1f376a
Sergey Krasnov <sa.krasnov@greenmoney.ru>
2022-10-14 17:59:25 +0700

    upd vars in env example

--- tools/env-setup-example.sh
+++ tools/env-setup-example.sh
@@ -1,5 +1,6 @@
-# Для удобства локальной отработки, пока нет CI/CD
+# Для удобства локальной разработки
 # Просто сделай: source env-setup-example.sh
+# При работе с пайплайном эти переменные необходимо поместить в переменные CI/CD
 
 export DB_HOST=postgresql.mcs.local
 export DB_PORT=5432
@@ -18,6 +19,20 @@ export OS_REGION_NAME="RegionOne"
 export OS_USER_DOMAIN_NAME="users"
 export OS_USERNAME="*********************"
 export OS_PASSWORD="*********************"
+
+export S3_BUCKET='figachechnaya'
+export S3_PATH='upload/'
+export S3_ENDPOINT='http://hb.bizmrg.com'
+export S3_FONT_LINK='https://bucket.hb.bizmrg.com/oldstandard.ttf'
+export S3_ACCESS_KEY='*********************'
+export S3_SECRET_KEY='*************************'
+
+export KAFKA_BOOTSTRAP='localhost:9092'
+export KAFKA_TOPIC_NAME='test'
+export KAFKA_GROUP_ID='imagizer'
+
+export FIGALIZE_API_PORT=9000
+export IMAGIZER_API_PORT=9001
 
 export TF_VAR_postgresql_db_name=$DB_LOGIN
 export TF_VAR_postgresql_db_user=$DB_PASS
